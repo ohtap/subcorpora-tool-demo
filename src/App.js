5f9c9b06f3ca43fb15e4edd92f03c614457f2d8a
unknown <hsun083@gmail.com>
2019-05-19 20:48:04 -0700

    Added default layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import loadable from '@loadable/component';
 import './App.css';
+
+const DefaultLayout = loadable(() => import('./containers/DefaultLayout'));
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DefaultLayout />
       </div>
     );
   }
